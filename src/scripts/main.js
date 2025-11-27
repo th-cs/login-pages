1342e7129641da6c48ea3f33e41c88767abdf96a
th-cs <202718515+th-cs@users.noreply.github.com>
2025-11-26 21:12:39 -0300

    feat: Add sign-up page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,18 @@ function passwordRecovery() {
 	alert("You will receive an email to recover your password!");
 }
 
+function signUp() {
+	alert("Signing up...");
+}
+
+function signUpGoogle() {
+	alert("Signing up with Google credentials...");
+}
+
+function privacyPolicy() {
+	alert("Opening privacy policy page...");
+}
+
 // Toggle password visibility
 
 let input = document.getElementById("password-input");
